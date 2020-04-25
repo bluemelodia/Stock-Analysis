6eeb9c2634d61080d150c84a4ac2b163fe3704b7
blue <mlh2197@columbia.edu>
2020-04-25 11:55:16 -0400

    feat: configure server.js to serve files from the client/views folder.

--- src/server/server.js
+++ src/server/server.js
@@ -11,6 +11,9 @@ const port = 3000;
 const fetch = require("node-fetch");
 const bodyParser = require('body-parser');
 
+/* Provides utilities for working with file and directory paths. */
+const path = require('path');
+
 /* Express v4+ requires a extra middle-ware layer to handle POST requests. 
  * Below is the configuration for express to use body-parser as middleware. */
 
@@ -37,8 +40,15 @@ app.use(cors());
  * As Express looks up the files relative to the static directory, 
  * the name of the static directory is not part of the URL. Note that
  * the path provided to the express.static function is relative to the 
- * direction from where the node process is launched. */
-app.use(express.static('website'));
+ * direction from where the node process is launched.
+ * 
+ * path.join uses path.normalize, which resolves . and .., handles multiple
+ * or trailing slashes, and uses the appropraite file separator for the platform.
+ * 
+ * __dirname gives the absolute path of the directory containing the currently 
+ * executing file. The value will depend on which file you invoke it in. */
+app.use(express.static(path.join(__dirname, '../client/views')));
+console.log(path.join(__dirname, '../client/views'))
 
 /* Spins up a simple local server that will allow the app to run 
  * locally in the browser. Tell the server which port to run on.
@@ -47,3 +57,7 @@ app.use(express.static('website'));
 const server = app.listen(port, () => {
     console.log(`running on localhost: ${port}`);
 }) 
+
+app.get('*', function(req, res) {
+    console.log("No other routes matched...");
+});
