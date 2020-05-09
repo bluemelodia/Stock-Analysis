bd6e27defc74947a2e9718f90bf0282e617feaaa
blue <mlh2197@columbia.edu>
2020-05-09 18:47:20 -0400

    fix: show alert on top of the insights panel.

--- src/client/js/news-service.js
+++ src/client/js/news-service.js
@@ -1,42 +1,48 @@
 import { currentDayAndTime } from './number-utils';
 import { advancedQuery, parseArticle } from './news-utils';
+import { alertType, showAlert } from '../index'
 
 let timeOfLastRequest;
 const requestLimit = 5000;
 
 const errorMessages = {
         FAILED_REQUEST: 'We were unable to fetch the news. Please try again.',
+        TOO_MANY_REQUESTS: 'You are making too many requests in a short period of time. Please wait a few seconds then try again.',
         TOO_MANY_REFRESHES: 'You are checking the news too often. Please wait a few minutes then try again.'
 }
 
 export async function findNews(symbol, name) {
+        const currentTime = Date.parse(currentDayAndTime());
+
+        if (timeOfLastRequest && currentTime - Date.parse(timeOfLastRequest) < requestLimit) {
+                console.log("Showing alert:");
+                showAlert(errorMessages.TOO_MANY_REQUESTS, alertType.warning);
+                return;
+        }
+        
         const everythingQuery = `+(${symbol} OR (${advancedQuery(name, ' AND ')}))`;
         const simpleQuery = advancedQuery(name);
 
         const breakingNews = await getNews(`http://localhost:3000/breakingNews/${simpleQuery}`);
+        console.log("Breaking news: ", breakingNews);
+
         if (breakingNews.statusCode !== 0) {
                 showAlert(errorMessages.FAILED_REQUEST, alertType.error);
         } else {
                 const allNews = await getNews(`http://localhost:3000/allNews/${everythingQuery}`);
+                console.log("All news: ", allNews);
                 if (allNews.statusCode !== 0) {
                         showAlert(errorMessages.FAILED_REQUEST, alertType.error);
                 }
         }
+        timeOfLastRequest = currentDayAndTime();
 }
 
 const getNews = async(url = '') => {
-        const currentTime = Date.parse(currentDayAndTime());
-
-        if (timeOfLastRequest && currentTime - Date.parse(timeOfLastRequest) < requestLimit) {
-                showAlert(errorMessages.TOO_MANY_REQUESTS, alertType.warning);
-                return;
-        }
-
         const response = await fetch(url);
 
         try {
                 const newsData = await response.json();
-                console.log("NEWS: ", newsData);
                 if (newsData.statusCode !== 0) {
                         throw newsData.errorMsg;
                 }
@@ -50,7 +56,6 @@ const getNews = async(url = '') => {
                                 continue;
                             }
                 }
-                console.log("news: ", news);
                 return { news: news, statusCode: 0};
         } catch (error) {
                 console.log("ERROR: ", error);
